6a0e442587bf6ea31913863bfc211fe7bcd2e66b
jqh <jqh@jqh-virtual-machine.(none)>
2014-12-08 17:21:18 +0800

    upup

--- db.py
+++ db.py
@@ -8,6 +8,7 @@ from sqlalchemy.pool import NullPool
 DBSession = sessionmaker(autoflush=True, expire_on_commit=True)
 
 def ConnectDB():
+    # DB_CONNECT_STRING = 'mysql+mysqldb://root:root@localhost/v2exfriends?charset=utf8'
     DB_CONNECT_STRING = 'mysql+mysqldb://root:jiazhang@localhost/v2exfriends?charset=utf8'
     engine = create_engine(DB_CONNECT_STRING, encoding='utf8', convert_unicode=True)
     DBSession.configure(bind=engine)
